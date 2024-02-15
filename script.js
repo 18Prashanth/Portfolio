ce8748303d031e29966ab7ffa7167baf46d6ac87
18Prashanth <18gowda2002@gmail.com>
2024-02-15 23:33:17 +0530

    completed

--- script.js
+++ script.js
@@ -40,4 +40,11 @@ window.onscroll = () => {
   //remove toggle icon and navbar when click navbar links
   menuIcon.classList.remove("bx-x");
   navbar.classList.remove("active");
+  //animation footer on scroll
+  let footer = document.querySelector("footer");
+
+  footer.classList.toggle(
+    "show-animate",
+    this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight
+  );
 };
